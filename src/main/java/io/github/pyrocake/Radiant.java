a2e7783ea6a1557db42c285788543103a6d91299
Pyrocake <38813517+Pyrocake@users.noreply.github.com>
2024-06-25 22:01:09 -0400

    Item handler in progress

--- src/main/java/io/github/pyrocake/Radiant.java
+++ src/main/java/io/github/pyrocake/Radiant.java
@@ -1,4 +1,25 @@
 package io.github.pyrocake;
 
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mod(Radiant.MOD_ID)
 public class Radiant {
+    public static final String MOD_ID = "radiant";
+    public static final Logger logger = LoggerFactory.getLogger(Radiant.class);
+
+    public Radiant(@NotNull IEventBus bus) {
+        bus.addListener(FMLClientSetupEvent.class, (fmlClientSetupEvent -> {
+            fmlClientSetupEvent.enqueueWork(() -> {
+                ModList.get().getModContainerById(MOD_ID).ifPresent(modContainer -> {
+                    logger.info("Waking up the sun, version: {}", modContainer.getModInfo().getVersion());
+                });
+            });
+        }));
+    }
 }
---
+++ src/main/java/io/github/pyrocake/init/ItemInit.java
@@ -0,0 +1,4 @@
+package io.github.pyrocake.init;
+
+public class ItemInit {
+}
