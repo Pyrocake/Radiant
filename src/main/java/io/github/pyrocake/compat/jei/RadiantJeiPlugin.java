6580e832cd7991c4bd7f9f275645486cd03e1127
Pyrocake <38813517+Pyrocake@users.noreply.github.com>
2026-07-24 02:06:28 -0400

    Expose solar oven smelting recipes in JEI

---
+++ src/main/java/io/github/pyrocake/compat/jei/RadiantJeiPlugin.java
@@ -0,0 +1,29 @@
+package io.github.pyrocake.compat.jei;
+
+import io.github.pyrocake.Radiant;
+import io.github.pyrocake.block.ModBlocks;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaRecipeTypes;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+@JeiPlugin
+public final class RadiantJeiPlugin implements IModPlugin {
+    private static final ResourceLocation PLUGIN_ID =
+            ResourceLocation.fromNamespaceAndPath(Radiant.MOD_ID, "jei_plugin");
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return PLUGIN_ID;
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(
+                new ItemStack(ModBlocks.SOLAR_OVEN_BLOCK.get()),
+                VanillaRecipeTypes.SMELTING
+        );
+    }
+}
