e9f5e02ea5164e4527d4ba633509af1078b81464
Pyrocake <38813517+Pyrocake@users.noreply.github.com>
2026-07-24 02:06:28 -0400

    Restore Jade tooltips and add JEI dev runtime

--- src/main/java/io/github/pyrocake/compat/jade/JadeCompat.java
+++ src/main/java/io/github/pyrocake/compat/jade/JadeCompat.java
@@ -7,20 +7,18 @@ import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
 import snownee.jade.api.WailaPlugin;
 
-//@WailaPlugin
-//public class JadeCompat implements IWailaPlugin {
-//
-//    @Override
-//    public void register(IWailaCommonRegistration registration) {
-//        //TODO register data providers
-//        registration.registerBlockDataProvider(SolarOvenProvider.INSTANCE, Solar_Oven_Block.class);
-//        registration.registerBlockDataProvider(CollectorProvider.INSTANCE, Collector_Block.class);
-//    }
-//
-//    @Override
-//    public void registerClient(IWailaClientRegistration registration) {
-//        //TODO register component providers, icon providers, callbacks, and config options here
-//        registration.registerBlockComponent(SolarOvenProvider.INSTANCE, Solar_Oven_Block.class);
-//        registration.registerBlockComponent(CollectorProvider.INSTANCE, Collector_Block.class);
-//    }
-//}
+@WailaPlugin
+public class JadeCompat implements IWailaPlugin {
+
+    @Override
+    public void register(IWailaCommonRegistration registration) {
+        registration.registerBlockDataProvider(SolarOvenProvider.INSTANCE, Solar_Oven_Block.class);
+        registration.registerBlockDataProvider(CollectorProvider.INSTANCE, Collector_Block.class);
+    }
+
+    @Override
+    public void registerClient(IWailaClientRegistration registration) {
+        registration.registerBlockComponent(SolarOvenProvider.INSTANCE, Solar_Oven_Block.class);
+        registration.registerBlockComponent(CollectorProvider.INSTANCE, Collector_Block.class);
+    }
+}
--- src/main/java/io/github/pyrocake/compat/jade/SolarOvenProvider.java
+++ src/main/java/io/github/pyrocake/compat/jade/SolarOvenProvider.java
@@ -1,51 +1,49 @@
 package io.github.pyrocake.compat.jade;
 
 import io.github.pyrocake.Radiant;
-import io.github.pyrocake.block.custom.Collector_Block;
 import io.github.pyrocake.block.custom.Solar_Oven_Block;
-import io.github.pyrocake.block.entity.CollectorBlockEntity;
 import io.github.pyrocake.block.entity.SolarOvenBlockEntity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.item.ItemStack;
 import snownee.jade.api.BlockAccessor;
 import snownee.jade.api.IBlockComponentProvider;
 import snownee.jade.api.IServerDataProvider;
 import snownee.jade.api.ITooltip;
 import snownee.jade.api.config.IPluginConfig;
-import snownee.jade.api.ui.BoxStyle;
-import snownee.jade.api.ui.IElementHelper;
-import snownee.jade.impl.ui.ProgressElement;
 
 import static io.github.pyrocake.block.custom.Solar_Oven_Block.INTENSITY;
-import static java.awt.Color.red;
 
-//public enum SolarOvenProvider implements IBlockComponentProvider, IServerDataProvider<BlockAccessor> {
-//    INSTANCE;
-//
-//    @Override
-//    public void appendTooltip(ITooltip iTooltip, BlockAccessor blockAccessor, IPluginConfig iPluginConfig) {
-//        if (blockAccessor.getServerData().contains("Intensity")) {
-//            iTooltip.add(new ProgressElement((blockAccessor.getServerData().getFloat("Intensity").get() / 15F),
-//                    Component.translatable("radiant.heatdisp", blockAccessor.getServerData().getInt("ChargeRate").get()),
-//                    IElementHelper.get().progressStyle().color(red.getRGB()).textColor(0xFFFFFF), BoxStyle.getNestedBox(), true));
-//        }
-//    }
-//
-//    @Override
-//    public ResourceLocation getUid() {
-//        return ResourceLocation.fromNamespaceAndPath(Radiant.MOD_ID, "solaroven");
-//    }
-//
-//    @Override
-//    public void appendServerData(CompoundTag data, BlockAccessor accessor) {
-//        Solar_Oven_Block block = (Solar_Oven_Block) accessor.getBlock();
-//        Level level = accessor.getLevel();
-//        data.putFloat("Intensity", block.intensity(level, accessor.getPosition()));
-//        BlockState state = accessor.getBlockEntity().getBlockState();
-//        data.putInt("ChargeRate", state.getValue(Solar_Oven_Block.INTENSITY));
-//    }
-//}
+public enum SolarOvenProvider implements IBlockComponentProvider, IServerDataProvider<BlockAccessor> {
+    INSTANCE;
+
+    @Override
+    public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
+        CompoundTag data = accessor.getServerData();
+        tooltip.add(Component.translatable("radiant.jade.heat", data.getInt("Intensity")));
+        int activeSlots = data.getInt("ActiveSlots");
+        if (activeSlots > 0) {
+            tooltip.add(Component.translatable("radiant.jade.cooking", activeSlots));
+        }
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+        return ResourceLocation.fromNamespaceAndPath(Radiant.MOD_ID, "solaroven");
+    }
+
+    @Override
+    public void appendServerData(CompoundTag data, BlockAccessor accessor) {
+        if (accessor.getBlockEntity() instanceof SolarOvenBlockEntity oven) {
+            data.putInt("Intensity", oven.getBlockState().getValue(INTENSITY));
+            int activeSlots = 0;
+            for (ItemStack stack : oven.getItems()) {
+                if (!stack.isEmpty()) {
+                    activeSlots++;
+                }
+            }
+            data.putInt("ActiveSlots", activeSlots);
+        }
+    }
+}
