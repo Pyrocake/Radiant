7249bbda6db7e28fefd866281268bdc683effed4
Pyrocake <38813517+Pyrocake@users.noreply.github.com>
2026-07-24 02:06:29 -0400

    Use JEI built-in smelting recipe type

--- src/main/java/io/github/pyrocake/compat/jei/RadiantJeiPlugin.java
+++ src/main/java/io/github/pyrocake/compat/jei/RadiantJeiPlugin.java
@@ -4,11 +4,10 @@ import io.github.pyrocake.Radiant;
 import io.github.pyrocake.block.ModBlocks;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
-import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.SmeltingRecipe;
 
 @JeiPlugin
 public final class RadiantJeiPlugin implements IModPlugin {
@@ -24,10 +23,7 @@ public final class RadiantJeiPlugin implements IModPlugin {
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
         registration.addRecipeCatalyst(
                 new ItemStack(ModBlocks.SOLAR_OVEN_BLOCK.get()),
-                RecipeType.createVanilla(
-                        "smelting",
-                        SmeltingRecipe.class
-                )
+                RecipeTypes.SMELTING
         );
     }
 }
