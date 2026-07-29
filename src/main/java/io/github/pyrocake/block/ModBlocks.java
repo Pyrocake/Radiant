47985976a23dba82d84a28c81294b8192936b56d
Pyrocake <38813517+Pyrocake@users.noreply.github.com>
2026-07-28 20:42:40 -0400

    Changed Block Map Color

--- src/main/java/io/github/pyrocake/block/ModBlocks.java
+++ src/main/java/io/github/pyrocake/block/ModBlocks.java
@@ -8,6 +8,7 @@ import io.github.pyrocake.block.custom.Sun_Block;
 import io.github.pyrocake.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -21,16 +22,15 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Radiant.MOD_ID);
 
-    //TODO Change MAP COLOR
-    public static final DeferredBlock<Block> PRISMALLON_BLOCK = registerBlockWithItem("prismallon_block", PrismallonBlock::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK));
-    public static final DeferredBlock<Block> RAW_PRISMALLON_BLOCK = registerBlockWithItem("raw_prismallon_block", RawPrismallonBlock::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK));
-    public static final DeferredBlock<Block> PRISMALLON_ORE_BLOCK = registerBlockWithItem("prismallon_ore", PrismallonOreBlock::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE));
-    public static final DeferredBlock<Block> DEEPSLATE_PRISMALLON_ORE_BLOCK = registerBlockWithItem("deepslate_prismallon_ore", DeepslatePrismallonOreBlock::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_IRON_ORE));
+    public static final DeferredBlock<Block> PRISMALLON_BLOCK = registerBlockWithItem("prismallon_block", PrismallonBlock::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).mapColor(MapColor.DIAMOND));
+    public static final DeferredBlock<Block> RAW_PRISMALLON_BLOCK = registerBlockWithItem("raw_prismallon_block", RawPrismallonBlock::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).mapColor(MapColor.DIAMOND));
+    public static final DeferredBlock<Block> PRISMALLON_ORE_BLOCK = registerBlockWithItem("prismallon_ore", PrismallonOreBlock::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_ORE).mapColor(MapColor.DIAMOND));
+    public static final DeferredBlock<Block> DEEPSLATE_PRISMALLON_ORE_BLOCK = registerBlockWithItem("deepslate_prismallon_ore", DeepslatePrismallonOreBlock::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_IRON_ORE).mapColor(MapColor.DIAMOND));
 
     public static final DeferredBlock<Sun_Block> SUN_BLOCK = registerBlockWithItem("sun_block", Sun_Block::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).noOcclusion());
     public static final DeferredBlock<Connector_Block> CONNECTOR_BLOCK = registerBlockWithItem("connector_block", Connector_Block::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).noOcclusion());
     public static final DeferredBlock<Collector_Block> COLLECTOR_BLOCK = registerBlockWithItem("collector_block", Collector_Block::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK));
-    public static final DeferredBlock<Solar_Oven_Block> SOLAR_OVEN_BLOCK = registerBlockWithItem("solar_oven_block", Solar_Oven_Block::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.FURNACE).noOcclusion());
+    public static final DeferredBlock<Solar_Oven_Block> SOLAR_OVEN_BLOCK = registerBlockWithItem("solar_oven_block", Solar_Oven_Block::new, () -> BlockBehaviour.Properties.ofFullCopy(Blocks.FURNACE).noOcclusion().mapColor(MapColor.COLOR_CYAN));
 
     public static <T extends Block> DeferredBlock<T> registerBlockWithItem(String name, Function<BlockBehaviour.Properties, T> block, Supplier<BlockBehaviour.Properties> properties) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, () -> block.apply(properties.get()));
